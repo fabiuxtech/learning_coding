dec48afe59efe0bf77383f20564fc174977c0401
fabiuxtech <fabio89@gmail.com>
2023-06-06 14:22:20 +0200

    now if there is no update the message is not sent

--- Python/tracking_message_to_telegram_bot/check_tracking_asendia.py
+++ Python/tracking_message_to_telegram_bot/check_tracking_asendia.py
@@ -5,17 +5,21 @@ from webdriver_manager.chrome import ChromeDriverManager
 import time
 from telegram import Bot
 import asyncio
-bot_token = 'mytokenbot'
+import warnings
+import os
+warnings.simplefilter('ignore', category=DeprecationWarning)
+bot_token = 'miotoken'
 # Imposta le opzioni del browser per eseguire Chromium in modalità headless
 chrome_options = Options()
 chrome_options.add_argument('--headless')  # Esegui in modalità headless senza interfaccia grafica
 # Imposta il percorso del driver di Chromium
-driver_path = '/home/pi/.wdm/drivers/chromedriver/linux64/114.0.5735.90/chromedriver'
+#driver_path = '/mnt/c/Users/fabiu/.wdm/drivers/chromedriver/win32/114.0.5735.90/chromedriver'
+driver_path = '/usr/bin/chromedriver'
 # Inizializza il driver di Chromium
 service = Service(driver_path)
 driver = webdriver.Chrome(service=service, options=chrome_options)
 #
-url = 'tracking_website'
+url = 'https://a1.asendiausa.com/tracking/?trackingnumber=EEUS001512506IT0'
 driver.get(url)
 time.sleep(2)
 #script = """
@@ -47,18 +51,49 @@ driver.quit()
 #
 n = 17
 filtered_results = results[n:]
-print(results[n:])
-chat_id = 'mychatid'
+#print(results[n:])
+
+chat_id = '32230559'
 custom_message = '<b>Tracking dal sito Asendia USA:</b>'
 message = custom_message + '\n' + '\n'.join(filtered_results)
-async def send_telegram_message(bot_token, chat_id, message):
-    bot = Bot(token=bot_token)
-    await bot.send_message(chat_id=chat_id, text=message, parse_mode='HTML')
 
-async def main():
-    bot_token_tg = bot_token
-    chat_id_tg = chat_id
-    message_tg = message
-    await send_telegram_message(bot_token_tg, chat_id_tg, message_tg)
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+def send_message():
+    async def send_telegram_message(bot_token, chat_id, message):
+        bot = Bot(token=bot_token)
+        await bot.send_message(chat_id=chat_id, text=message, parse_mode='HTML')
+    async def main():
+        bot_token_tg = bot_token
+        chat_id_tg = chat_id
+        message_tg = message
+        await send_telegram_message(bot_token_tg, chat_id_tg, message_tg)
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(main())
+
+asenda_txt = '/home/fabiuxtech/coding/learning_coding/Python/tracking_message_to_telegram_bot/asenda.txt'
+list_to_str = ' '.join(filtered_results)
+if os.path.exists(asenda_txt):
+    file = open('asenda.txt', 'r')
+    read_file_before = file.read()
+    if read_file_before == list_to_str:
+        print(f'I risultati sono identici quindi non procedo.')
+        file.close()
+    else:
+        print(f'Le informazioni sono diverse quindi procedo con l\'update')
+        file=open('asenda.txt', 'w')
+        file.write(list_to_str)
+        file.close()
+        file=open('asenda.txt', 'r')
+        read_file_after = file.read()
+        print(read_file_after)
+        file.close()
+else:
+    print('Non ho informazioni, quindi procedo con l\'update')
+    send_message()
+    file=open('asenda.txt', 'w')
+    file.write(list_to_str)
+    file.close()
+    file=open('asenda.txt', 'r')
+    read_file_after = file.read()
+    print(read_file_after)
+    file.close()
--- Python/tracking_message_to_telegram_bot/check_tracking_dpd.py
+++ Python/tracking_message_to_telegram_bot/check_tracking_dpd.py
@@ -5,32 +5,23 @@ from webdriver_manager.chrome import ChromeDriverManager
 import time
 from telegram import Bot
 import asyncio
-bot_token = 'mytokenbot'
+import warnings
+import os
+warnings.simplefilter('ignore', category=DeprecationWarning)
+bot_token = 'miotoken'
 # Imposta le opzioni del browser per eseguire Chromium in modalità headless
 chrome_options = Options()
 chrome_options.add_argument('--headless')  # Esegui in modalità headless senza interfaccia grafica
 # Imposta il percorso del driver di Chromium
-driver_path = '/home/pi/.wdm/drivers/chromedriver/linux64/114.0.5735.90/chromedriver'
+driver_path = '/usr/bin/chromedriver'
 # Inizializza il driver di Chromium
 service = Service(driver_path)
 driver = webdriver.Chrome(service=service, options=chrome_options)
 #
-url = 'tracking_website'
+url = 'https://tracking.dpd.de/status/en_US/parcel/29130000071255'
 driver.get(url)
 time.sleep(2)
-#script = """
-#var results = [];
-#
-#// Codice JavaScript utilizzando jQuery
-#$("span.asendia-lead").each(function() {
-#    var spanText = $(this).text().trim();
-#    var divText = $(this).next('.card-body').text().trim();
-#    var combinedText = spanText + ' ' + divText;
-#    results.push(combinedText);
-#});
-#
-#return results;
-#"""
+
 script = """
 var results = [];
 
@@ -47,25 +38,50 @@ results = driver.execute_script(script)
 # Chiudi il driver
 driver.quit()
 #
-#n = 17
-#filtered_results = results[n:]
-#print(results[n:])
-chat_ids = ['mychatid', 'otherchatid']
+chat_ids = ['32230559', '137293063']
+#nicola id 137293063
 custom_message = '<b>Tracking dal sito DPD Group (BRT):</b>'
 message = custom_message + '\n' + '\n'.join(results)
-async def send_telegram_message(bot_token, chat_id, message):
-    bot = Bot(token=bot_token)
-    await bot.send_message(chat_id=chat_id, text=message, parse_mode='HTML')
 
-async def main():
-    bot_token_tg = bot_token
-    tasks = []
-    for chat_id in chat_ids:
-        task = send_telegram_message(bot_token_tg, chat_id, message)
-        tasks.append(task)
-    await asyncio.gather(*tasks)
-    #chat_id_tg = chat_id
-    #message_tg = message
-    #await send_telegram_message(bot_token_tg, chat_id_tg, message_tg)
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+def send_message():
+    async def send_telegram_message(bot_token, chat_id, message):
+        bot = Bot(token=bot_token)
+        await bot.send_message(chat_id=chat_id, text=message, parse_mode='HTML')
+    async def main():
+        bot_token_tg = bot_token
+        tasks = []
+        for chat_id in chat_ids:
+            task = send_telegram_message(bot_token_tg, chat_id, message)
+            tasks.append(task)
+        await asyncio.gather(*tasks)
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(main())
+
+dpd_txt = '/home/fabiuxtech/coding/learning_coding/Python/tracking_message_to_telegram_bot/dpd.txt'
+list_to_str = ' '.join(results)
+if os.path.exists(dpd_txt):
+    file = open('dpd.txt', 'r')
+    read_file_before = file.read()
+    if read_file_before == list_to_str:
+        print(f'I risultati sono identici quindi non procedo.')
+        file.close()
+    else:
+        print(f'Le informazioni sono diverse quindi procedo con l\'update')
+        file=open('dpd.txt', 'w')
+        file.write(list_to_str)
+        file.close()
+        file=open('dpd.txt', 'r')
+        read_file_after = file.read()
+        print(read_file_after)
+        file.close()
+else:
+    print('Non ho informazioni, quindi procedo con l\'update')
+    send_message()
+    file=open('dpd.txt', 'w')
+    file.write(list_to_str)
+    file.close()
+    file=open('dpd.txt', 'r')
+    read_file_after = file.read()
+    print(read_file_after)
+    file.close()
