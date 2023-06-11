e21e5c7436837d16237a8c4d8d3a1ef9496c6f3b
fabiuxtech <fabio89@gmail.com>
2023-06-11 10:48:38 +0200

    .

---
+++ Python/encoding_decoding_ex/enc_dec_ex.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+
+#/start testing
+
+#/end testing
+
+def encodeString(stringVal):
+    prevChar = None
+    count_char = 0
+    encoding = []
+    for char in stringVal:
+        if char != prevChar:
+            count_char = 1
+            print(f'New character is {char}')
+            newChar = (char, count_char)
+            encoding.append(newChar)
+        prevChar = char
+    return encoding
+
+def decodeString(encodeList):
+    pass
+
+art = '''
+
+                                                                                
+                                                                                
+                               %%%%%%%%%%%%%%%%%%%                              
+                        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%                       
+                    %%%%%%%%                         %%%%%%%%                   
+                %%%%%%%                                   %%%%%%                
+              %%%%%%                                         %%%%%%             
+           %%%%%%                                               %%%%%           
+          %%%%%                                                   %%%%%         
+        %%%%%                                                       %%%%%       
+       %%%%                 %%%%%              %%%%%                  %%%%      
+      %%%%                 %%%%%%%            %%%%%%%                  %%%%     
+     %%%%                  %%%%%%%            %%%%%%%                   %%%%    
+    %%%%                   %%%%%%%            %%%%%%%                    %%%%   
+    %%%%                    %%%%%              %%%%%                     %%%%   
+   %%%%                                                                   %%%%  
+   %%%%                                                                   %%%%  
+   %%%%                                                                   %%%%  
+   %%%%                                                      %%%%        %%%%   
+    %%%%       %%%%%%                                        %%%%%       %%%%   
+    %%%%         %%%%                                       %%%%        %%%%    
+     %%%%         %%%%                                     %%%%         %%%%    
+      %%%%         %%%%%                                  %%%%         %%%%     
+       %%%%%         %%%%%                             %%%%%         %%%%%      
+        %%%%%          %%%%%%                        %%%%%          %%%%        
+          %%%%%           %%%%%%%               %%%%%%%           %%%%%         
+            %%%%%             %%%%%%%%%%%%%%%%%%%%%             %%%%%           
+              %%%%%%%                                        %%%%%              
+                 %%%%%%%                                 %%%%%%%                
+                     %%%%%%%%%                     %%%%%%%%%                    
+                          %%%%%%%%%%%%%%%%%%%%%%%%%%%%%                         
+                                   %%%%%%%%%%%%                                 
+                                                                                
+                                                                                 
+
+'''
+#encodeString = encodeString(art)
+stringVal = 'AAAAABBBBAAA'
+print(encodeString(stringVal))
