ed7ccc67f7c11d03b714b192b40f10571942e5b0
fabiuxtech <fabio89@gmail.com>
2023-06-15 22:16:26 +0200

    created all functions and cases

--- Python/calculator/calculator.py
+++ Python/calculator/calculator.py
@@ -28,6 +28,17 @@ def square(x):
     tot = sqrt(x)
     return tot
 
+def isNumber(x,y='1'):
+    if x.isdigit() and y.isdigit():
+        return 0
+    else:
+        return 1
+
+def stringToNumber(x,y='1'):
+    x = int(x)
+    y = int(y)
+    return x,y
+
 def pause():
     x = input('Vuoi continuare? (S/N)')
     x = x.lower
@@ -49,19 +60,68 @@ operations = '''
     7) Esci dal programma
     '''
 
-print(sw)
+print('\n',sw)
 print(operations)
 
 while True:
     choice = input('\nScegli l\'operazione --> ')
+    print('\n')
     match choice:
         case '1':
-            print('Hai scelto somma:')
-            x = int(input('Inserisci primo numero? '))
-            y = int(input('Inserisci il secondo numero '))
-            print(f'La somma di {x} + {y} è {sum(x,y)}')
+            print('Hai scelto Somma:')
+            x = input('Inserisci il primo numero: ')
+            y = input('Inserisci il secondo numero: ')
+            if isNumber(x,y) == 1:
+                print(f'\nDevi inserire solo numeri\nHai inserito {x} e {y}')
+            else:
+                sTn = stringToNumber(x,y)
+                print(f'La somma di {sTn[0]} + {sTn[1]} è {sum(sTn[0],sTn[1])}')
+        case '2':
+            print('Hai scelto Sottrazione:')
+            x = input('Inserisci il primo numero: ')
+            y = input('Inserisci il secondo numero: ')
+            if isNumber(x,y) == 1:
+                print(f'\nDevi inserire solo numeri\nHai inserito {x} e {y}')
+            else:
+                sTn = stringToNumber(x,y)
+                print(f'La differenza tra {sTn[0]} - {sTn[1]} è {sub(sTn[0],sTn[1])}')
+        case '3':
+            print('Hai scelto Moltiplicazione')
+            x = input('Inserisci il primo numero: ')
+            y = input('Inserisci il secondo numero: ')
+            if isNumber(x,y) == 1:
+                print(f'\nDevi inserire solo numeri\nHai inserito {x} e {y}')
+            else:
+                sTn = stringToNumber(x,y)
+                print(f'Il risultato tra {sTn[0]} * {sTn[1]} è {mult(sTn[0],sTn[1])}')
+        case '4':
+            print('Hai scelto Divisione:')
+            x = input('Inserisci il primo numero: ')
+            y = input('Inserisci il secondo numero: ')
+            if isNumber(x,y) == 1:
+                print(f'\nDevi inserire solo numeri\nHai inserito {x} e {y}')
+            else:
+                sTn = stringToNumber(x,y)
+                print(f'Il risultato di {sTn[0]} : {sTn[1]} è {div(sTn[0],sTn[1])}')
+        case '5':
+            print('Hai scelto Elevamento a Potenza:')
+            x = input('Inserisci il numero da elevare: ')
+            y = input('Inserisci l\'esponenente: ')
+            if isNumber(x,y) == 1:
+                print(f'\nDevi inserire solo numeri\nHai inserito {x} e {y}')
+            else:
+                sTn = stringToNumber(x,y)
+                print(f'Il risultato di {sTn[0]} elevato a {sTn[1]} è {power(sTn[0],sTn[1])}')
+        case '6':
+            print('Hai scelto Radice Quadrata:')
+            x = input('Inserisci il numero sul quale calcolare la radice quadrata: ')
+            if isNumber(x) == 1:
+                print(f'\nDevi inserire solo numeri\nHai inserito {x} e {y}')
+            else:
+                sTn = stringToNumber(x)
+                print(f'La radice quadrata di {sTn[0]} è {sqrt(sTn[0])}')
         case '7':
-            print(f'Grazie per aver usato {sw}, a presto!\n')
+            print(f'\nGrazie per aver usato {sw}, a presto!\n')
             break
         case _:
             print('Scelta non corretta')
