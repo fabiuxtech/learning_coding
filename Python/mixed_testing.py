05a9f462f6f53dc58fe2c35086404dd9d172b917
fabiuxtech <fabio89@gmail.com>
2023-07-24 20:52:14 +0200

    testing

--- Python/mixed_testing.py
+++ Python/mixed_testing.py
@@ -11,16 +11,27 @@
 #else:
 #    print('Yuck! That\'s not it!')
 #print('Thanks for playing!')
-def favorite_city(name):
-    print('One of my favorite cities is', name)
+# def favorite_city(name):
+#     print('One of my favorite cities is', name)
 
-favorite_city('Roma')
-favorite_city('Tokyo')
-favorite_city('New York')
+# favorite_city('Roma')
+# favorite_city('Tokyo')
+# favorite_city('New York')
 
-def isEven(num):
-    return num % 2 == 0
-if isEven(3):
-    print('3 is even')
-else: 
-    print('3 is not even')
+# def isEven(num):
+#     return num % 2 == 0
+# if isEven(3):
+#     print('3 is even')
+# else: 
+#     print('3 is not even')
+    
+# Test potenze alla 0
+from math import sqrt
+a = 3
+b = 3
+c = a**b
+if (a or b) == 0:
+    print("operazione non definita")
+else:
+    print(c)
+sqrt
