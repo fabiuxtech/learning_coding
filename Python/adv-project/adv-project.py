ffeb5630384e657a3304a05f5db263173fa6dc7f
fabiuxtech <fabio89@gmail.com>
2023-09-15 19:42:34 +0200

    just playing around

--- Python/adv-project/adv-project.py
+++ Python/adv-project/adv-project.py
@@ -11,19 +11,29 @@ h = "help"
 inv = "inventory"
 def exiting():
     pass
+class iNventory:
+    def __init__(self) -> None:
+        pass
+    def empty():
+        print(f"Inventory is empty")
+    def full():
+        print(f"Inventory is Full")
+    def listing():
+        print(f"Those are your items:")
 def inventory():
     if invent == {}:
         print(f"Your inventory is: empty")
     else:
         print(f"Your inventory is: {invent}")
 def backtoprompt():
-    print(f"What's your next move? > ")
-    choices(input())
+    print(f"What's your next move?")
+    choices(input(f"=> "))
 def choices(where):
     where = str.lower(where)
     match where:
         case "h":
-            print(f"""This is help section...
+            print(f"""
+                  This is help section...
                   Your available choices:
                     s = {s}
                     n = {n}
@@ -32,9 +42,14 @@ def choices(where):
                     i = {inv}
                     h = {h}
                   """)
-            choices(input())
+            choices(input(f"=> "))
         case "i":
-            inventory()
+            if invent == {}:
+                iNventory.empty()
+            elif invent == "Full":
+                iNventory.full()
+            else:
+                iNventory.listing()
         case "s":
             print(f"You're going {s}...")
 print(f"Welcome to {land}\nType 'h' for {h}")
