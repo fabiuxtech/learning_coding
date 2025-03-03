316cdc0ea31c4a75c237d9746d1be05f94f6f5e5
fabiuxtech <fabio89@gmail.com>
2025-03-03 09:50:25 +0100

    removed file

--- bash/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-source ambiente.var
-echo $plan
-echo $clientgroup
-echo $proxy1
-echo $proxy2
-echo $cshost
-sed -e "s/\VAR_PLAN/$plan/" -e "s/\VAR_CLIENTGROUP/$clientgroup/" -e "s/\VAR_PROXY1/$proxy1/" -e "s/\VAR_PROXY2/$proxy1/" -e "s/\VAR_CSHOST/$cshost/" template.txt > new_template.txt
