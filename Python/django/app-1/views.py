24b3f783cf93f3e53827f44bba11e0bfb2cc08ca
Fabio <fabio89@gmail.com>
2025-03-17 14:31:20 +0100

    Delete Python/django/app-1/views.py

--- Python/django/app-1/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-from datetime import datetime, date, time, timezone
-
-def currentDT(request):
-    now = datetime.now()
-    now = f"{now.day}/{now.month}/{now.year} - {now.hour}:{now.minute}:{now.second}"
-    html = '<html lang="en"><body>Today is %s.</body></html>' % now
-    return HttpResponse(html)
